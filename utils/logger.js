2e55cfaccb8dc162b59b29ff6ebb72c504516cb2
mhshohug <mh.shohug@gmail.com>
2026-07-24 04:31:23 +0600

    Add files via upload

---
+++ utils/logger.js
@@ -0,0 +1,44 @@
+const winston = require('winston');
+
+// Define log levels and colors
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+};
+
+const colors = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  http: 'magenta',
+  debug: 'white',
+};
+
+winston.addColors(colors);
+
+// Define log format
+const format = winston.format.combine(
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
+  winston.format.colorize({ all: true }),
+  winston.format.printf(
+    (info) => `[${info.timestamp}] [${info.level}]: ${info.message}`
+  )
+);
+
+// Define transports
+const transports = [
+  new winston.transports.Console(),
+];
+
+// Create the logger
+const logger = winston.createLogger({
+  level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
+  levels,
+  format,
+  transports,
+});
+
+module.exports = logger;
